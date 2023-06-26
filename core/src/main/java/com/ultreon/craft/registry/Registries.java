7e2b1c01b3870218772958039858d5f1f6534938
Qboi123 <qboiwastaken@gmail.com>
2023-06-26 13:29:20 +0200

    Improve registries

--- core/src/main/java/com/ultreon/craft/registry/Registries.java
+++ core/src/main/java/com/ultreon/craft/registry/Registries.java
@@ -1,6 +1,5 @@
 package com.ultreon.craft.registry;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.ultreon.craft.UltreonCraft;
 import com.ultreon.craft.audio.SoundEvent;
 import com.ultreon.craft.block.Block;
@@ -10,14 +9,19 @@ import com.ultreon.craft.world.gen.noise.NoiseSettings;
 import com.ultreon.libs.registries.v0.Registry;
 
 public final class Registries {
+    public static final Registry<Registry<?>> REGISTRIES = Registry.create(UltreonCraft.id("registry"));
     public static final Registry<Block> BLOCK = Registry.create(UltreonCraft.id("block"));
     public static final Registry<NoiseSettings> NOISE_SETTINGS = Registry.create(UltreonCraft.id("noise_settings"));
     public static final Registry<EntityType<?>> ENTITIES = Registry.create(UltreonCraft.id("entity_type"));
-    public static final Registry<Registry<?>> REGISTRY = Registry.create(UltreonCraft.id("registry"));
     public static final Registry<Font> FONTS = Registry.create(UltreonCraft.id("font"));
     public static final Registry<SoundEvent> SOUNDS = Registry.create(UltreonCraft.id("sound"));
 
-    public static void nopInit() {
-
+    public static void init() {
+        REGISTRIES.register(REGISTRIES.id(), REGISTRIES);
+        REGISTRIES.register(BLOCK.id(), BLOCK);
+        REGISTRIES.register(NOISE_SETTINGS.id(), NOISE_SETTINGS);
+        REGISTRIES.register(ENTITIES.id(), ENTITIES);
+        REGISTRIES.register(FONTS.id(), FONTS);
+        REGISTRIES.register(SOUNDS.id(), SOUNDS);
     }
 }
